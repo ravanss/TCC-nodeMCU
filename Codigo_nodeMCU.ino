843bb66196ebcf7c3c8d2bd8502d2af1acabff5b
ravanss <psaulo7@gmail.com>
2018-11-11 10:16:04 -0200

    Ajuste no codigo final, para uso em geral da aplicação

--- Codigo_nodeMCU.ino
+++ Codigo_nodeMCU.ino
@@ -4,12 +4,12 @@
 #include <Ticker.h> //Biblioteca responsavel por publicar os dados coletados no nodeMCU para o firebase
 
 //Definição dos dados do firebase.
-#define FIREBASE_HOST "teste-69708.firebaseio.com"    //Locar onde recebe os dados da aplicação.
-#define FIREBASE_AUTH "OIVJ8Xv2SVvc6XeVQaLVjiqVF89jJf2NfUnEwV8X"    //Atenticando no servidor do firebase.
+#define FIREBASE_HOST "*"    //Locar onde recebe os dados da aplicação.
+#define FIREBASE_AUTH "*"    //Atenticando no servidor do firebase.
 
 //Definição dos dados da rede local
-#define WIFI_SSID "FATEC LABs" //nome da rede wifi.
-#define WIFI_PASSWORD "ceetepspg"  //senha de rede wifi.
+#define WIFI_SSID "*" //nome da rede wifi.
+#define WIFI_PASSWORD "*"  //senha de rede wifi.
 
 // Definiçao para o tempo de interevalo do envio dos dados
 #define PUBLISH_INTERVAL 1000*60*5 
@@ -37,7 +37,7 @@ pinMode(pino_sinal_analogico, INPUT);
 // leitura pino D0 - chuva
 pinMode(pino_sinal_digital, INPUT);
   Serial.begin(9600);
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD); //enviando o nome da rede ea senha  para conectar.
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD); //enviando o nome da rede e a senha para conectar  internet.
   Serial.print("connecting"); //mensagem caso a conecção de certo.
   while (WiFi.status() != WL_CONNECTED) { //caso a de certo..
     Serial.print(".");  //mostra um . para sinalizar que esta carregando
@@ -46,7 +46,7 @@ pinMode(pino_sinal_digital, INPUT);
   Serial.println();
   Serial.print("connected: ");  //mostra que conectou
   Serial.println(WiFi.localIP()); //mostrando o ip do esp8266 na rede
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH); //enviando os dados do firebase pela rede ja conectada 
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH); //enviando as permições pela rede 
   // Registra o ticker para publicar de tempos em tempos
   ticker.attach_ms(PUBLISH_INTERVAL, publish); //Enviando os dados do coletados para o firebase guarda
 }
@@ -58,18 +58,16 @@ void loop() {
   valor_digital = analogRead(pino_sinal_digital);
   // Apenas publique quando passar o tempo determinado
   if(publishNewState){
-    Serial.println("Publish new State");
+    Serial.println("Publish new State"); // uma mensagem para saber que vai ocorrer a publicação dos dados
     // Obtem os dados do sensor DHT 
-    //float humidity = ;
-    if(!isnan(valor_analogico)){
-      // Manda sinal umidade para o FB
+    if(!isnan(valor_analogico)){ // se o valor for numerico execute...
       StaticJsonBuffer<200> jsonBuffer; // Instacio a ultilização da classe jsonBuffer; 
-      JsonObject& root = jsonBuffer.createObject(); // criando um objeto do memso tipo da classe para ser salvo no firebase
+      JsonObject& root = jsonBuffer.createObject(); // criando um objeto do mesmo tipo da classe para ser salvo no firebase
       //neste momento uma key(chave) foi criadano firebase, nela vai conter um arquivo no formato json.
       root["umidade"] = valor_analogico; // root umidade sera o primeiro valor
       root["chuva"] = valor_digital;  // root chuva sera o segundo valor
       // append a new value to /logDHT
-      String name = Firebase.push("sensor", root); // aqui os dados do root são enviados
+      String name = Firebase.push("sensor", root); // os dados do root são enviados
       Serial.println("Valor umidade:"); 
       Serial.println(valor_analogico); // exibindo o valor na serial
       Serial.println("Valor chuva:");
@@ -78,6 +76,6 @@ void loop() {
       Serial.println("Error Publishing"); //caso ocorra alguns erro para publicar o dado sera apresentada uma mensagem na saida serial
     }
   }
-  delay(5000); // de quanto em quanto tempo sera feita a leitura do dados e o envio do mesmo
+  delay(5000); // de quanto em quanto tempo sera feita a leitura do dados
   // ----- Fim
 }
